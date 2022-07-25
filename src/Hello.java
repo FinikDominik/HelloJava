353c8ad6b9b9f029798aa4ec3667c05249802374
Dominik Fínik <finikdominik@gmail.com>
2022-07-25 17:08:21 +0200

    afd

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int myFisrtNumber = (19999 - 1999) / 6;
+        int mySecondNumber = 66;
+        int myThirdNumber = myFisrtNumber / 100;
+        int myTotal = myFisrtNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+    }
 }
